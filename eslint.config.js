f5260060d9c735377bce7c421041b9de3e35035d
Noam Okman <noamokman@gmail.com>
2025-03-16 17:53:32 +0200

    .

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import { baseConfig, testConfig } from '@osskit/eslint-config';
+import { baseConfig } from '@osskit/eslint-config';
 import typescriptEslint from 'typescript-eslint';
 
 export default typescriptEslint.config({ ignores: ['dist/**'] }, { ...baseConfig, files: ['src/**/*.ts'] });
